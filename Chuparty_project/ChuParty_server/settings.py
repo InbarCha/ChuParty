d5093e1833c02b9e112ef3e7fbd0fb30f2bc97a6
bar.kazzaz <bar.kazzaz@imperva.com>
2020-04-09 06:51:01 +0300

    removed 'rest_framework_mongoengine'

--- Chuparty_project/ChuParty_server/settings.py
+++ Chuparty_project/ChuParty_server/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'frontend.apps.FrontendConfig',
-    'rest_framework_mongoengine'
+    'frontend.apps.FrontendConfig'
 ]
 
 MIDDLEWARE = [
