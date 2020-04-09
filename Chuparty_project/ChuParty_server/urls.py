f6811cc1f8128fa636490b21a3ba1c82c7431e02
bar.kazzaz <bar.kazzaz@imperva.com>
2020-04-09 08:41:39 +0300

    urls.py handles as a SPA

--- Chuparty_project/ChuParty_server/urls.py
+++ Chuparty_project/ChuParty_server/urls.py
@@ -15,10 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.views.generic import TemplateView
-from django.urls import include, path
+from django.contrib.auth.decorators import login_required
+from django.urls import include, path, re_path
+
 
 urlpatterns = [
     # path('', include('frontend.urls')),
+    #TODO: handle login_required
+    #TODO: add apis path
     path('', TemplateView.as_view(template_name='index.html')),
+    re_path(r'^(?:.*)/?$', TemplateView.as_view(template_name="index.html")),# any path that is not '/'
     path('admin/', admin.site.urls)
 ]
---
+++ Chuparty_project/frontend/src/components/Main.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Home extends Component{
+    render(){
+        return(
+            <div>
+                <h2>Hello from Main!</h2>
+            </div>
+        )
+    }
+}
