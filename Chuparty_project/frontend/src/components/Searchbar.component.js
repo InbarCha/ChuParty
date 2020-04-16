2fcb4bbcc7a2589c94e5ba285c555efe2a86fa47
bar.kazzaz <bar.kazzaz@imperva.com>
2020-04-16 13:30:44 +0300

    searchbar with logo

--- Chuparty_project/frontend/src/components/Searchbar.component.js
+++ Chuparty_project/frontend/src/components/Searchbar.component.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-
+import Logo from './../assets/logo.png';
 export default class Home extends Component{
     render(){
         return(
             <div className="title">
+                <img src={Logo} alt="logo"/>
                 <span>ChuParty</span>
                 <div className="search-container">
                     <form action="/action_page.php">
