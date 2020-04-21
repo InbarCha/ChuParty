45443181ae2d0bcf45855b2d6198bc1be40981e5
bar.kazzaz <bar.kazzaz@imperva.com>
2020-04-21 22:20:40 +0300

    main fetches schools

--- Chuparty_project/frontend/src/components/Main.component.js
+++ Chuparty_project/frontend/src/components/Main.component.js
@@ -1,11 +1,47 @@
 import React, { Component } from 'react';
+const SCHOOLS_ROUTE  = (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') ?
+    'http://localhost:8000/api/getSchools' : '/api/getSchools';
 
-export default class Main extends Component{
-    render(){
-        return(
-            <div>
-                <span>Main</span>
-            </div>
-        )
+export default class Main extends Component {
+    constructor() {
+        super();
+        this.state = {
+            schools: null
+        }
+    }
+    componentDidMount() {
+        /* schools should look like the following:
+        //
+        { 
+            "name": "Computer Science",
+            "courses": [
+                {
+                "name": "Computer Networks",
+                "subjects": [
+                    {
+                        "name": "HTTP"
+                    },
+                    {
+                        "name": "UDP"
+                    },
+                    {
+                        "name": "DNS"
+                    }
+                ]
+                }
+            ]
+        }
+        */
+        fetch(SCHOOLS_ROUTE)
+            .then(res => res.json())
+            .then(data => { console.log(data); this.setState({ schools: data }) })
+            .catch(err => console.error("error while fetching schools:", err));
+    }
+    getSchoolsArr() {
+        return (<div>loaded :)</div>)
+    }
+    render() {
+        let res = (this.state.schools !== null) ? this.getSchoolsArr() : <div><span>Loading Schools..</span></div>;
+        return (res)
     }
 }
