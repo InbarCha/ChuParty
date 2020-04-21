202839b8f4073625459bf42060121fa9d4285edc
bar.kazzaz <bar.kazzaz@imperva.com>
2020-04-21 23:11:57 +0300

    courses according to new DB topology

--- Chuparty_project/frontend/src/components/Courses.component.js
+++ Chuparty_project/frontend/src/components/Courses.component.js
@@ -11,32 +11,24 @@ export default class Courses extends Component {
         }
     }
     componentDidMount() {
-        // course should look like the following:
-        // name: "OOP"
-        // subjects: Array(1)
-        // 0: {name: "Object-Oriented Principles"}
+        // courses should look like the following:
+        // [{"OOP": {subjects: ["Object-Oriented Principles"]}}, ...]
         fetch(COURSES_ROUTE)
             .then(res => res.json())
             .then(data => { console.log(data); this.setState({ courses: data }) })
             .catch(err => console.error("error while fetching courses:", err));
     }
     getCoursesArr() {
-        return this.state.courses.map((elm, index) => {
-            return (
-                <div key={index} className={"course"}>
-                    <span>{elm.name}:</span>
-                    <div className="subjects_container">
-                        {elm.subjects.map((sub, j_index) => {
-                            return (
-                                <div key={j_index} className={"course_subject"}>
-                                    {sub.name}
-                                </div>
-                            )
-                        })}
-                    </div>
+        return (this.state.courses.map((elm, index) => {
+            let k = Object.keys(elm)[0];
+            return <div key={index} className={"course"}>{k}
+                <div className="subjects_container">
+                    {elm[k]["subjects"].map((elm, j_index)=>{
+                        return <div key={j_index}>{elm}</div>
+                    })}
                 </div>
-            )
-        });
+            </div>
+        }))
     }
     render() {
         let res = (this.state.courses !== null) ? this.getCoursesArr() : <div><span>Loading Courses..</span></div>;
