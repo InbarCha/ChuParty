5c77ab131aa8b198569758862dc7626f532f9ce2
bar.kazzaz <bar.kazzaz@imperva.com>
2020-05-13 17:09:06 +0300

    searchbar now forwards the searched string

--- Chuparty_project/frontend/src/components/Searchbar.component.js
+++ Chuparty_project/frontend/src/components/Searchbar.component.js
@@ -1,18 +1,34 @@
 import React, { Component } from "react";
 import Logo from "./../assets/logo.png";
 export default class Home extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchInput: ''
+    }
+  }
+  submitSearch(e) {
+    this.props.filterBy(this.state.searchInput);
+  }
+
+  updateSearchInput(e) {
+    this.setState({ searchInput: e.target.value })
+    this.props.filterBy(e.target.value);
+  }
+
   render() {
     return (
       <div className="title">
         <img src={Logo} alt="logo" />
         <span>ChuParty</span>
         <div className="search-container">
-          <form action="/action_page.php">
-            <input type="text" placeholder="Search..." name="search" />
-            <button type="submit">
+          <div id='searchBtnContainer'>
+            <input type="text" placeholder="Search..." onChange={this.updateSearchInput.bind(this)} />
+            <button onClick={this.submitSearch.bind(this)}>
               <i className="material-icons">search</i>
             </button>
-          </form>
+          </div>
         </div>
       </div>
     );
