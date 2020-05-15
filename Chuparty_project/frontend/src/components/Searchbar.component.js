36798b43b88d632159c19956dc2cea813ce3714c
InbarCha <Inbar@LAPTOP-IDK7GB0E>
2020-05-15 15:32:49 +0300

    adding account icon

--- Chuparty_project/frontend/src/components/Searchbar.component.js
+++ Chuparty_project/frontend/src/components/Searchbar.component.js
@@ -1,19 +1,18 @@
 import React, { Component } from "react";
 import Logo from "./../assets/logo.png";
 export default class Home extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      searchInput: ''
-    }
+      searchInput: "",
+    };
   }
   submitSearch(e) {
     this.props.filterBy(this.state.searchInput);
   }
 
   updateSearchInput(e) {
-    this.setState({ searchInput: e.target.value })
+    this.setState({ searchInput: e.target.value });
     this.props.filterBy(e.target.value);
   }
 
@@ -23,13 +22,18 @@ export default class Home extends Component {
         <img src={Logo} alt="logo" />
         <span>ChuParty</span>
         <div className="search-container">
-          <div id='searchBtnContainer'>
-            <input type="text" placeholder="Search..." onChange={this.updateSearchInput.bind(this)} />
+          <div id="searchBtnContainer">
+            <input
+              type="text"
+              placeholder="Search..."
+              onChange={this.updateSearchInput.bind(this)}
+            />
             <button onClick={this.submitSearch.bind(this)}>
               <i className="material-icons">search</i>
             </button>
           </div>
         </div>
+        <span class="material-icons account_icon">account_circle</span>
       </div>
     );
   }
