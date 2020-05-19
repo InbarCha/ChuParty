6f6a3ee9ee97465bfa24bc5d43aac1d57b1960e5
InbarCha <Inbar@LAPTOP-IDK7GB0E>
2020-05-19 19:16:55 +0300

    Adding design for profile page

--- Chuparty_project/frontend/src/components/Auth/Profile.component.js
+++ Chuparty_project/frontend/src/components/Auth/Profile.component.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
+import { MDBContainer, MDBRow, MDBCol, MDBBadge } from "mdbreact";
+import { Row, Col } from "react-bootstrap";
 
 export class Profile extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      username: localStorage["username"],
+      username: localStorage["loggedUsername"],
       first_name: localStorage["logged_first_name"],
       last_name: localStorage["logged_last_name"],
       email: localStorage["logged_email"],
@@ -15,7 +17,71 @@ export class Profile extends Component {
 
   render() {
     return (
-      <div>Hello {this.state.first_name + " " + this.state.last_name}!</div>
+      <div>
+        <Row>
+          <Col
+            xl={{ span: 4, offset: 4 }}
+            lg={{ span: 6, offset: 3 }}
+            md={{ span: 6, offset: 3 }}
+            sm={{ span: 8, offset: 2 }}
+            className="login_wrapper"
+          >
+            <MDBContainer>
+              <MDBRow>
+                <MDBCol className="col col-cen">
+                  <form className="login_form">
+                    <h1>
+                      <MDBBadge color="primary">Profile</MDBBadge>
+                    </h1>
+                    <br />
+                    <h5>
+                      <MDBBadge color="info">Your Username</MDBBadge>
+                    </h5>
+                    <label
+                      htmlFor="defaultFormLoginEmailEx"
+                      className="grey-text"
+                    >
+                      {this.state.username}
+                    </label>
+                    <br />
+                    <h5>
+                      <MDBBadge color="info">Your First Name</MDBBadge>
+                    </h5>
+                    <label
+                      htmlFor="defaultFormLoginPasswordEx"
+                      className="grey-text"
+                    >
+                      {this.state.first_name}
+                    </label>
+                    <br />
+                    <h5>
+                      <MDBBadge color="info">Your Last Name</MDBBadge>
+                    </h5>
+                    <label
+                      htmlFor="defaultFormLoginPasswordEx"
+                      className="grey-text"
+                    >
+                      {this.state.last_name}
+                    </label>
+                    <br />
+                    <h5>
+                      <MDBBadge color="info">Your Email</MDBBadge>
+                    </h5>
+                    <label
+                      htmlFor="defaultFormLoginPasswordEx"
+                      className="grey-text"
+                    >
+                      {this.state.email}
+                    </label>
+                    <br />
+                    <img className="login_img" alt="not found" />
+                  </form>
+                </MDBCol>
+              </MDBRow>
+            </MDBContainer>
+          </Col>
+        </Row>
+      </div>
     );
   }
 }
